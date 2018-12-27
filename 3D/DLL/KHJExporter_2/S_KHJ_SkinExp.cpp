5db7e905c62ffa4af83aa958d01bb729cc9f50f3
현진 김 <hyunjin9173@gmail.com>
2018-12-27 18:58:29 +0900

    asd

--- 3D/DLL/KHJExporter_2/S_KHJ_SkinExp.cpp
+++ 3D/DLL/KHJExporter_2/S_KHJ_SkinExp.cpp
@@ -116,6 +116,188 @@ void S_KHJ_SkinExp::ExportPhysiqueData(INode* node, Modifier *phyMod)
 	phyMod->ReleaseInterface(I_PHYINTERFACE, phyExport);
 }
 
+void  S_KHJ_SkinExp::ExportSkinData(INode* node, Modifier *skinMod)
+{
+	if (node == NULL || skinMod == NULL)return;
+	// get the skin interface
+	ISkin * skin = (ISkin*)skinMod->GetInterface(I_SKIN);
+	ISkinContextData* skinData = skin->GetContextInterface(node);
+
+	if (skin && skinData)
+	{
+		int numberOFPoints;
+		// 스킨 버텍스의 갯수
+		numberOFPoints = skinData->GetNumPoints();
+		for (int i = 0; i < numberOFPoints; i++)
+		{
+			// 연결된 본의 갯수
+			int numOFWeights = skinData->GetNumAssignedBones(i);
+
+			SBipedVertex BipVertex;
+			BipVertex.m_dwNumWeight = numOFWeights;
+
+			for (int j = 0; j < numOFWeights; j++)
+			{
+				int boneIndex = skinData->GetAssignedBone(i, j);
+				INode* pBone = skin->GetBone(boneIndex);
+				if (pBone == NULL)break;
+
+				const TCHAR* nodeName = nodeName = pBone->GetName(); //skin - >GetBoneName(boneIndex);
+				float boneWeight = skinData->GetBoneWeight(i, j);
+
+				BYTE Index = (BYTE)I_Writer.GetIndex(nodeName);
+				BipVertex.m_BipIDList.push_back(Index);
+				BipVertex.m_fWeightList.push_back(boneWeight);
+
+				if (Index < 0)
+				{
+					MessageBox(GetActiveWindow(), _T("No Biped ID"), _T("Error!!"), MB_OK | MB_ICONSTOP);
+					break;
+				}
+			}
+			m_BipedList.push_back(BipVertex);
+
+			// 최대 가중치 갯수를 저장
+			if (m_Scene.iMaxweight < numOFWeights)
+			{
+				m_Scene.iMaxweight = numOFWeights;
+				if (m_Scene.iMaxweight > 8)
+				{
+					MessageBox(GetActiveWindow(), _T(" 바이패드 + 본 + 더미가 정점 당 8개 이상 적용되었습니다."), _T("최대 적용 에러 !"), MB_OK | MB_ICONSTOP);
+				}
+			}
+		}
+	}
+}
+
+void S_KHJ_SkinExp::SetBipedInfo(INode* node)
+{
+	Modifier *phyMod = FindModifier(node, Class_ID(PHYSIQUE_CLASS_ID_A, PHYSIQUE_CLASS_ID_B));
+	Modifier *skinMod = FindModifier(node, SKIN_CLASSID);
+	if (phyMod)
+	{
+		ExportPhysiqueData(node, phyMod);
+	}
+	else if (skinMod)
+	{
+		ExportSkinData(node, skinMod);
+	}
+}
+// 노드에 대한 정보 획득
+bool S_KHJ_SkinExp::GetNodeInfo(INode* node, TimeValue t)
+{
+	// 랜더가능한 데이터 정보 버퍼 저장.
+	BOOL needDel;
+	TriObject *tri = I_Writer.GetTriObjectFromNode(node, t, needDel);
+	if (tri == NULL)return false;
+
+	Mesh * mesh = &tri->GetMesh();
+	mesh->buildNormals();
+	if (!mesh->faces || !tri || !mesh || !mesh->getNumFaces() || !mesh->getNumVerts() || !mesh->getNumTVerts())
+	{
+		return false;
+	}
+
+	SetBipedInfo(node);
+
+	SSkinMesh* SkinMesh = NULL;
+	SAFE_NEW(SkinMesh, SSkinMesh);
+	// 메쉬를 저장한다. (서브메터리얼 여부를 판단하여 저장)
+	GenerateGroup(node, mesh, SkinMesh);
+
+	if (needDel)
+	{
+		delete tri;
+	}
+	return true;
+}
+
+void S_KHJ_SkinExp::SetVertexBiped(INode* node, Face* face, int v0, int v1, int v2, SSkinTri* pTri)
+{
+	int iNumWeight = 1;
+	if (m_BipedList.size())
+	{
+		iNumWeight = m_BipedList[face->v[v0]].m_BipIDList.size();
+		for (int iBip = 0; iBip < iNumWeight; iBip++)
+		{
+			if (iBip < 4)
+			{
+				pTri->m_vVertex[0].i1[iBip] = m_BipedList[face->v[v0]].m_BipIDList[iBip];
+				pTri->m_vVertex[0].w1[iBip] = m_BipedList[face->v[v0]].m_fWeightList[iBip];
+			}
+			else
+			{
+				pTri->m_vVertex[0].i2[iBip - 4] = m_BipedList[face->v[v0]].m_BipIDList[iBip];
+				pTri->m_vVertex[0].w2[iBip - 4] = m_BipedList[face->v[v0]].m_fWeightList[iBip];
+			}
+		}
+		iNumWeight = m_BipedList[face->v[v2]].m_BipIDList.size();
+		for (int iBip = 0; iBip < iNumWeight; iBip++)
+		{
+			if (iBip < 4)
+			{
+				pTri->m_vVertex[1].i1[iBip] = m_BipedList[face->v[v2]].m_BipIDList[iBip];
+				pTri->m_vVertex[1].w1[iBip] = m_BipedList[face->v[v2]].m_fWeightList[iBip];
+			}
+			else
+			{
+				pTri->m_vVertex[1].i2[iBip - 4] = m_BipedList[face->v[2]].m_BipIDList[iBip];
+				pTri->m_vVertex[1].w2[iBip - 4] = m_BipedList[face->v[2]].m_fWeightList[iBip];
+			}
+		}
+		iNumWeight = m_BipedList[face->v[v1]].m_BipIDList.size();
+		for (int iBip = 0; iBip < iNumWeight; iBip++)
+		{
+			if (iBip < 4)
+			{
+				pTri->m_vVertex[2].i1[iBip] = m_BipedList[face->v[v1]].m_BipIDList[iBip];
+				pTri->m_vVertex[2].w1[iBip] = m_BipedList[face->v[v1]].m_fWeightList[iBip];
+			}
+			else
+			{
+				pTri->m_vVertex[2].i2[iBip - 4] = m_BipedList[face->v[v1]].m_BipIDList[iBip];
+				pTri->m_vVertex[2].w2[iBip - 4] = m_BipedList[face->v[v1]].m_fWeightList[iBip];
+			}
+		}
+		// 정점 당 가중치 적용 갯수
+		pTri->m_vVertex[0].w2[3] = m_BipedList[face->v[v0]].m_dwNumWeight;
+		pTri->m_vVertex[1].w2[3] = m_BipedList[face->v[v2]].m_dwNumWeight;
+		pTri->m_vVertex[2].w2[3] = m_BipedList[face->v[v1]].m_dwNumWeight;
+	}
+	else
+	{
+		// 정점에 적용된 바이패드가 없다는 것은 자체 애니메이션을 가진 오브젝트가 된다.
+		// 이때는 가중치를 얻을 수 없기 떄문에 직접 1개의 가중치 값을 세팅해 주어야 한다.
+		int iBipIndex = I_Writer.GetIndex(node->GetName());
+		pTri->m_vVertex[0].i1[0] = iBipIndex;
+		pTri->m_vVertex[0].w1[0] = 1.0f;
+		pTri->m_vVertex[1].i1[0] = iBipIndex;
+		pTri->m_vVertex[1].w1[0] = 1.0f;
+		pTri->m_vVertex[2].i1[0] = iBipIndex;
+		pTri->m_vVertex[2].w1[0] = 1.0f;
+
+		// 가중치 적용 갯수
+		pTri->m_vVertex[0].w2[3] = 1.0f;
+		pTri->m_vVertex[1].w2[3] = 1.0f;
+		pTri->m_vVertex[2].w2[3] = 1.0f;
+	}
+}
+
+void S_KHJ_SkinExp::SetTriangle(INode* node, Mesh* mesh, SSkinMesh* pSkinMesh, SSkinMesh*pSubMesh, int iMtrl)
+{
+	SSkinMesh* pMesh = NULL;
+	if (pSubMesh == NULL)
+	{
+		pMesh = pSkinMesh;
+	}
+	else
+	{
+		pMesh = pSubMesh;
+	}
+
+	// 원하는 시점 (I_Writer.m_p3dsMax->GetTime() 에서 월드 행렬을 얻는다.
+	Matrix3 tm = node->GetObjTMAfterWSM(I_Writer.m_Interval.Start());
+}
 
 S_KHJ_SkinExp::S_KHJ_SkinExp()
 {
--- 3D/DLL/KHJExporter_2/S_KHJ_Writer.h
+++ 3D/DLL/KHJExporter_2/S_KHJ_Writer.h
@@ -1,8 +1,31 @@
 #pragma once
-class S_KHJ_Writer
+#include "S_KHJ_AnimObj.h"
+
+class S_KHJ_Writer : public S_KHJ_AnimObj, public TSingleton<S_KHJ_Writer>
 {
+private:
+	friend class TSingleton<S_KHJ_Writer>;
+public:
+	FILE* m_pStream; // 출력 파일 포인터
+	TSTR m_strFileName; // 출력 할 파일명
+public:
+	bool Init(TSTR strFileName, Interface* pMax); // 초기화
+	bool KHJExport(); // 파일 출력
+	bool Release(); // 메모리 소멸 및 해제
+	// 출력 파일 지정 대화상자
+	TCHAR* SaveFileDlg(TCHAR* szExt, TCHAR* szTitle = _T("KHJExporter"));
+	void ExpScene();
+	void ExpMaterial();
+	void ExpObject();
+	int ExpMesh(FILE* fp, SMesh* pMesh, int iMtrl = -1, int iAddCount = 0);
+	void ExpAnimation(SMesh* pMesh);
+	void ExpTexture(vector<STexMap>& TexMap);
+	int IsEqulVertexList(vector<PNCT_VERTEX>& VertexArray, PNCT_VERTEX& Vertex);
+	int SetUniqueBuffer(SMesh* pMesh, int iMtrl, int iStartTir, vector<PNCT_VERTEX>& VertexArray, vector<int>& IndexArray);
+	void matExport();
 public:
 	S_KHJ_Writer();
 	virtual ~S_KHJ_Writer();
 };
+#define I_Writer S_KHJ_Writer::GetInstance()
 
