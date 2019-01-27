24a2af9bfc7af9dbb408c7a97c278f9e1b152664
leader1118 <hyunjin9173@gmail.com>
2019-01-28 03:27:59 +0900

    gn

--- 3D/DLL/KHJViewer/S_Character.h
+++ 3D/DLL/KHJViewer/S_Character.h
@@ -2,8 +2,21 @@
 #include "xstd.h"
 class S_Character
 {
+public:   
+	T_STR m_szName;
+	int m_iStartFrame;
+	int m_iLastFrame;
+	int m_iCurrentFrame; 
+	float m_fFrame;
+	float m_fLerpTime;
+	D3DXMATRIX m_pMatrix[255];
+	int m_iMatrixIndex;
+	ID3D11Buffer* m_pBoneBuffer;
+	ID3D11ShaderResourceView* m_pBoneBufferRV;
 public:
-
+	bool Init();
+	bool Frame();
+	bool 
 public:
 	S_Character();
 	virtual ~S_Character();
